1045dbb53923ff06348080a282305e3f6b95fe15
Janne Spijkervet <janne.spijkervet@gmail.com>
2019-07-31 15:40:41 +0100

    changes

--- setup_home.sh
+++ setup_home.sh
@@ -1,25 +1,17 @@
+#!/bin/bash
 set -o allexport; source .env; set +o allexport
-
-sudo mkdir -p /docker/nextcloud/redis
-sudo mkdir -p /docker/postgresql
-sudo mkdir -p /docker/odoo/addons
-sudo mkdir -p /docker/organizr/config
-
-sudo mkdir -p /docker/media/shared/movies
-sudo mkdir -p /docker/media/shared/tv
-
 docker network create traefik
 
-docker-compose -f traefik/docker-compose.yml \
-    -f postgres/docker-compose.yml \
-    -f odoo/docker-compose.yml \
-    -f watchtower/docker-compose.yml \
-    -f media/docker-compose.yml \
-    up -d
+docker-compose -f traefik/docker-compose.yml up -d
+docker-compose -f postgres/docker-compose.yml up -d 
+docker-compose -f odoo/docker-compose.yml up -d 
+docker-compose -f watchtower/docker-compose.yml up -d 
+docker-compose -f media/docker-compose.yml up -d 
+
 
 cd nextcloud && docker-compose up -d && cd ../
 
-sudo chown -R $USER:docker /docker/media
+# sudo chown -R $PUID:$PGID /docker/media
 
 # docker stack deploy --compose-file traefik/docker-compose.yml \
 #     -c postgres/docker-compose.yml \
